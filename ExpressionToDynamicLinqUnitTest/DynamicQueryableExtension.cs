fe91627d41c545b6e4ce8519dd2bad6761e7401c
毛才君 <mccj@MCCJ-PC>
2019-07-19 17:58:18 +0800

    1

--- ExpressionToDynamicLinqUnitTest/DynamicQueryableExtension.cs
+++ ExpressionToDynamicLinqUnitTest/DynamicQueryableExtension.cs
@@ -8,7 +8,7 @@ namespace System.Linq
 {
     public static class DynamicQueryableExtension
     {
-#if NETSTANDARD2_0
+#if NETCOREAPP2_0
         public static IEnumerable<T> Where<T>(this IEnumerable<T> source, string predicate, string defaultValue = null)
         {
             var s = source.AsQueryable();
